caf01d3b1e36b832fc0eb82ebaefbf18a5750220
Marius Hamre Nordrik <marius@nordrik.com>
2017-09-05 14:31:44 +0200

    Stub

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from "react-dom";
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Link to="/login">LOGIN</Link>
+    );
+  }
+}
+
+export default App;
